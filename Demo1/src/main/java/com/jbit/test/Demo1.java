39110b24aace3a7509f5654da68da3d71048f0ae
purpleclp <605937926@example.com>
2019-11-18 21:37:38 +0800

    Demo1 提交

---
+++ Demo1/src/main/java/com/jbit/test/Demo1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.jbit.test;
+
+/**
+ * @author 崔大大
+ *
+ */
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("加油");
+	}
+
+}
